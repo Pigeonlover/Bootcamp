765770c4da7a3fa4508ae445ab317820221fd45f
Pigeonlover <bravefoot@outlook.com>
2025-08-19 14:04:41 +0100

    Functions workshop

---
+++ Week-2/Day-2/Functions/functions.js
@@ -0,0 +1,34 @@
+// Functions
+
+let a = 10;
+let b = 5;
+
+// ADD
+function add() {
+  console.log(a + b);
+}
+add();
+
+// SUBTRACT
+function subtract() {
+  console.log(a - b);
+}
+subtract();
+
+// MULTIPLY
+function multiply() {
+  console.log(a * b);
+}
+multiply();
+
+// DIVIDE
+function divide() {
+  console.log(a / b);
+}
+divide();
+
+// REMAINDER
+function remainder() {
+  console.log(a % b);
+}
+remainder();
